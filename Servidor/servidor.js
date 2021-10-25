d22e9be69d8e66e8387492c43122bed986ad52fa
mati <matiasalmeida546@gmail.com>
2021-10-24 23:15:20 -0300

    Servidor iniciado

--- Servidor/servidor.js
+++ Servidor/servidor.js
@@ -1,7 +1,34 @@
-// SERVIDOR MANDA ESTO AL PRIMER TRACKER PARA HACER UN STORE DE UN ARCHIVO NUEVO.
+const express = require('express');
+
+const server = express();
+
+server.use(express.json());
+
+server.post('/file', (req, res) => {
+    console.log(req.body);
+});
+
+server.get('/file', (req, res) => {
+    // buscar la lista completa de archivas y devolverla
+    res.send('Lista de archivos escaneados');
+});
+
+server.get('/file/:hash', (req, res) => {
+    //busca el archivo en los trackers y devuelve el .torrent con la lista de pares que tienen el archivo
+    res.send('Devuelve .torrente')
+})
+
+
+server.listen(4200, () => {
+    console.log('Escuchando 4200');
+})
+
+
+
+/* SERVIDOR MANDA ESTO AL PRIMER TRACKER PARA HACER UN STORE DE UN ARCHIVO NUEVO.
 
 route
-/*
+
 {
     route: /file/{hash}/store
     body: {
@@ -12,9 +39,6 @@ route
         nodePort: int
     }
 }
-
-*/
-
 CASE:
 
     ALTA: A TRACKER
@@ -26,8 +50,6 @@ CASE:
         nodeIP: str,
         nodePort: int
     }
-    
 
     LISTAR:
-
-    SOLICITUD DESCARGA:
+    SOLICITUD DESCARGA: */
