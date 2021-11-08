32f1f30f4273f335f108cf6762ace2e33a1a4729
Nacho-C <nachomercado@live.com>
2021-11-08 00:00:00 -0300

    scan + lista de archivos en webclient

--- Servidor/public/client.js
+++ Servidor/public/client.js
@@ -11,7 +11,7 @@ function doupload() {
     //open the request
     xhr.open('POST', 'http://190.245.254.237:27016/file')
     xhr.setRequestHeader("Content-Type", "application/json");
-
+	event.preventDefault();
     //send the form data
     xhr.send(package);
 }
@@ -23,7 +23,23 @@ function scan(){
     xhr.setRequestHeader("Content-Type", "application/json");
     xhr.onreadystatechange = function() {
         if (xhr.readyState === 4) {
-          console.log(xhr.response);
+			let listaArchivos = document.getElementById("archivos");
+			listaArchivos.innerHTML = ''; //elimina la lista previa
+			let archivos = JSON.parse(xhr.response).sort((a,b)=>{ //ordena los archivos alfabéticamente y luego por tamaño
+				if (a.filename.toUpperCase() < b.filename.toUpperCase())
+					return -1;
+				else
+					if (a.filename.toUpperCase() == b.filename.toUpperCase() && a.filesize <= b.filesize)
+						return -1;
+					else
+						return 1;
+			});
+			archivos.forEach((e,i,array)=>{ //agrega los archivos a la lista en el html
+				let elemento = document.createElement("li");
+				elemento.setAttribute('id',e.filename);
+				elemento.appendChild(document.createTextNode(e.filename + " - " + e.filesize + " bytes"));
+				listaArchivos.appendChild(elemento);
+			});
         }
       }
     //send the form data
