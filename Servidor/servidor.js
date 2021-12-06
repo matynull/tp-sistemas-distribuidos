80599b4b8d0ae404d6eb5247a8850c99aac1a86d
Nacho-C <nachomercado@live.com>
2021-12-06 14:22:15 -0300

    Join y Update - Prueba

--- Servidor/servidor.js
+++ Servidor/servidor.js
@@ -3,15 +3,16 @@ const path = require('path');
 const net = require('net')
 const udp = require('dgram');
 const crypto = require('crypto');
-const { SourceMap } = require('module');
-const { response } = require('express');
 
 const server = express();
 server.use(express.json());
 server.use(express.static('public'));
 server.set('trust proxy', true);
+let puertoServerCliente;
 
 let msgID = 1;
+
+var msgPendientes = []; // vector para ID-Respuesta 
 
 server.post('/file', async (req, res) => {
     let indice;
@@ -20,10 +21,10 @@ server.post('/file', async (req, res) => {
     let aux = req.ip.split(':');
     formulario.nodeIP = aux[aux.length - 1];
     await store(formulario);
-    indice = respuestasID.findIndex((e) => e.id == idSave);
-    while (respuestasID[indice].Response === undefined)
+    indice = msgPendientes.findIndex((e) => e.id == idSave);
+    while (msgPendientes[indice].mensaje === undefined)
         await delay(50);
-    res.send(JSON.stringify(respuestasID[indice].Response)); //Respuesta al POST
+    res.send(JSON.stringify(msgPendientes[indice].mensaje)); //Respuesta al POST
     //LUQUI AAAAAAAAAAA AGARRÁ ESE JSON Y HACE UN CARTELITO CON EL STATUS
 });
 
@@ -31,17 +32,15 @@ server.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public', 'index.html'));
 })
 
-var respuestasID = []; // vector para ID-Respuesta 
-
 server.get('/file', async (req, res) => {
     // buscar la lista completa de archivas y devolverla
     let indice;
     let idSave = msgID;
     await scan();
-    indice = respuestasID.findIndex((e) => e.id == idSave);
-    while (respuestasID[indice].Response === undefined)
+    indice = msgPendientes.findIndex((e) => e.id == idSave);
+    while (msgPendientes[indice].mensaje === undefined)
         await delay(50);
-    res.send(JSON.stringify(respuestasID[indice].Response));
+    res.send(JSON.stringify(msgPendientes[indice].mensaje));
 });
 
 function delay(delay) {
@@ -55,61 +54,61 @@ server.get('/file/:hash', async (req, res) => {
     let indice;
     let idSave = msgID;
     await search(req.params.hash);
-    indice = respuestasID.findIndex((e) => e.id == idSave);
-    while (respuestasID[indice].Response === undefined)
+    indice = msgPendientes.findIndex((e) => e.id == idSave);
+    while (msgPendientes[indice].mensaje === undefined)
         await delay(50);
-    res.send(JSON.stringify(respuestasID[indice].Response));
-})
-
-
-server.listen(27016,'0.0.0.0', () => {
-    console.log('Escuchando 27016');
+    res.send(JSON.stringify(msgPendientes[indice].mensaje));
 })
 
 //--------------------------------------------------------- TRACKER MANAGEMENT ---------------------------------------------------------//
 
 let socket = udp.createSocket('udp4');
-const puertoSV = 27017
+let puertoServerTrackers, ipTracker, puertoTracker, idTracker;
+
 
 socket.on('message', function (msg, info) {
-    //console.log("me llegó un mensaje");
     let mensaje = JSON.parse(msg.toString());
     let mensajeID = mensaje.messageId;
-    let indexRespuesta = respuestasID.findIndex((e) => e.id == mensajeID);
-    if (indexRespuesta != -1) {
-        if (mensaje.route == '/scan')
-            respuestasID[indexRespuesta] = {
+    let indice = msgPendientes.findIndex((e) => e.id == mensajeID);
+    if (indice != -1) {
+        if (mensaje.route === '/scan')
+            msgPendientes[indice] = {
                 id: mensajeID,
-                Response: mensaje.body.files
+                mensaje: mensaje.body.files
             }
-        else {
+        else if (mensaje.route === '/join') {
+            mensaje.trackerIP = info.address;
+            mensaje.id = parseInt(encriptado.update(mensaje.trackerIP + ':' + mensaje.trackerPort).digest('hex').substring(0, 2), 16);
+            socket.send(JSON.stringify(mensaje),puertoTracker,ipTracker,(err) => {
+                console.log("Hubo un error al enviar Join al primer Tracker.");
+            });
+            if (ipTracker === '0.0.0.0' || mensaje.id < idTracker) {
+                ipTracker = mensaje.trackerIP;
+                puertoTracker = mensaje.trackerPort;
+                idTracker = mensaje.id;
+            }
+        } else
             if (mensaje.route.includes('/found'))
-                respuestasID[indexRespuesta] = {
+                msgPendientes[indice] = {
                     id: mensajeID,
-                    Response: {
+                    mensaje: {
                         hash: mensaje.body.id,
                         trackerIP: info.address,
                         trackerPort: info.port
                     }
                 }
-            else if (mensaje.route.includes('/store')) { //Confirmación de Store
-                //LUQUI HACE UN CARTELITO EN LA PÁGINA ES UNA ORDEN
-                respuestasID[indexRespuesta] = {
-                    id:mensajeID,
-                    Responde: mensaje // Mando mensaje completo, podría mandar solo status
+            else if (mensaje.route.includes('/store')) { //Confirmación de Store - ???
+                msgPendientes[indice] = {
+                    id: mensajeID,
+                    mensaje: mensaje // Mando mensaje completo, podría mandar solo status
                 }
                 console.log("Llego mensaje de confirmación de Store con id " + mensajeID + ": " + mensaje.status);
             }
-        }
     } else {
         console.log("Llegó un mensaje con ID desconocido.");
     }
-    console.log(respuestasID[indexRespuesta]);
+    console.log(msgPendientes[indice]);
 });
-
-socket.bind(puertoSV);
-const portTracker = 27015; //CFGEAR ESTO PLS
-const ipTracker = '190.190.36.80';
 
 function store(formulario) {
     return new Promise((resolve) => {
@@ -119,7 +118,7 @@ function store(formulario) {
             messageId: msgID,
             route: '/file/' + hash + '/store',
             originIP: '0.0.0.0',
-            originPort: puertoSV,
+            originPort: puertoServerTrackers,
             body: {
                 id: hash,
                 filename: formulario.filename,
@@ -130,14 +129,14 @@ function store(formulario) {
         }
         socket.send(JSON.stringify(objetoStore), portTracker, ipTracker, (err) => {
             if (!err) {
-                respuestasID.push({ id: msgID });
-                msgID+=2;
+                msgPendientes.push({ id: msgID });
+                msgID += 2;
             } else
                 console.log('Error al enviar petición Store.');
             resolve();
         });
     });
-}
+};
 
 function scan() {
     return new Promise((resolve) => {
@@ -145,17 +144,17 @@ function scan() {
             messageId: msgID,
             route: '/scan',
             originIP: '0.0.0.0',
-            originPort: puertoSV,
+            originPort: puertoServerTrackers,
         }), portTracker, ipTracker, function (err) {
             if (!err) {
-                respuestasID.push({ id: msgID });
-                msgID+=2;
+                msgPendientes.push({ id: msgID });
+                msgID += 2;
             } else
                 console.log('error en send de scan');
             resolve();
         })
     })
-}
+};
 
 function search(hash) {
     return new Promise((resolve) => {
@@ -163,14 +162,32 @@ function search(hash) {
             messageId: msgID,
             route: '/file/' + hash,
             originIP: '0.0.0.0',
-            originPort: puertoSV
+            originPort: puertoServerTrackers
         }), portTracker, ipTracker, function (err) {
             if (!err) {
-                respuestasID.push({ id: msgID });
-                msgID+=2;
+                msgPendientes.push({ id: msgID });
+                msgID += 2;
             } else
                 console.log('error en send de search');
             resolve();
         })
     })
-}
+};
+
+function leerCfg() {
+    const config = JSON.parse(fs.readFileSync('./servidor.cfg'));
+    puertoServerTrackers = config.puertoServerTrackers;
+    puertoServerCliente = config.puertoServerCliente;
+    ipTracker = config.ipTracker;
+    puertoTracker = config.puertoTracker;
+    let encriptado = crypto.createHash('sha1');
+    idTracker = parseInt(encriptado.update(ipTracker + ':' + puertoTracker).digest('hex').substring(0, 2), 16);
+};
+
+leerCfg();
+
+server.listen(puertoServerCliente, '0.0.0.0', () => {
+    console.log('Escuchando ' + puertoServerCliente);
+});
+
+socket.bind(puertoServerTrackers);
